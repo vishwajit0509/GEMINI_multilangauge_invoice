2cb7453a66244c5f114c27dfe5cfe43e325b9411
Vishwajit <vishwajitsarakpatil@email.com>
2025-03-20 18:29:19 +0530

    summary

---
+++ YOUTUBESUM/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from dotenv import load_dotenv
+
+load_dotenv() ##load all the nevironment variables
+import os
+import google.generativeai as genai
+
+from youtube_transcript_api import YouTubeTranscriptApi
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
